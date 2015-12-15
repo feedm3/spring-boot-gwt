3920af5335c7ee36c7d1cf4898554db10eb2111a
Fabian Dietenberger <piano.fabian@t-online.de>
2015-12-15 22:33:12 +0100

    Add simple todo panel

--- src/main/java/com/codecrafters/client/TodoPanel.java
+++ src/main/java/com/codecrafters/client/TodoPanel.java
@@ -1,9 +1,14 @@
 package com.codecrafters.client;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.dom.client.Document;
+import com.google.gwt.dom.client.LIElement;
+import com.google.gwt.dom.client.UListElement;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
-import com.google.gwt.user.client.ui.FlowPanel;
-import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.HTMLPanel;
 import org.fusesource.restygwt.client.Method;
 import org.fusesource.restygwt.client.MethodCallback;
 
@@ -14,22 +19,37 @@ import java.util.List;
  */
 public class TodoPanel extends Composite {
 
-    private final TodoItemService service = GWT.create(TodoItemService.class);
+    interface TestViewUiBinder extends UiBinder<HTMLPanel, TodoPanel> {}
+    private static TestViewUiBinder ourUiBinder = GWT.create(TestViewUiBinder.class);
+
+    private static final TodoItemService todoItemService = GWT.create(TodoItemService.class);
+
+    @UiField
+    Button showTodoItemsButton;
+
+    @UiField
+    UListElement todoList;
 
     public TodoPanel() {
-        final FlowPanel rootPanel = new FlowPanel();
-        initWidget(rootPanel);
+        HTMLPanel rootElement = ourUiBinder.createAndBindUi(this);
+        initWidget(rootElement);
 
-        service.getTodos("", new MethodCallback<List<TodoItem>>() {
-            @Override
-            public void onFailure(final Method method, final Throwable exception) {
-                rootPanel.add(new Label(exception.getMessage()));
-            }
+        showTodoItemsButton.addClickHandler(event -> {
+            todoItemService.getTodos("", new MethodCallback<List<TodoItem>>() {
+                @Override
+                public void onFailure(final Method method, final Throwable exception) {
 
-            @Override
-            public void onSuccess(final Method method, final List<TodoItem> response) {
-                rootPanel.add(new Label(response.toString()));
-            }
+                }
+
+                @Override
+                public void onSuccess(final Method method, final List<TodoItem> response) {
+                    for (final TodoItem todoItem : response) {
+                        final LIElement todoListItem = Document.get().createLIElement();
+                        todoListItem.setInnerHTML(todoItem.getText());
+                        todoList.appendChild(todoListItem);
+                    }
+                }
+            });
         });
     }
 }
