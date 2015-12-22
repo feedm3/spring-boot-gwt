f305720c7ea21fce7555b9c7fa9f554c4ca6913c
Fabian Dietenberger <fabian.dietenberger@ametras.com>
2015-12-22 09:22:38 +0100

    Add test for scheduledDatabaseResetTask

---
+++ src/test/groovy/com/codecrafters/server/service/ScheduledDatabaseResetTaskTest.groovy
@@ -0,0 +1,42 @@
+package com.codecrafters.server.service
+
+import com.codecrafters.server.domain.TodoItemRepository
+import spock.lang.Specification
+/**
+ * This class is used as test for the ScheduledDatabaseResetTask.
+ *
+ * @author Fabian Dietenberger
+ */
+class ScheduledDatabaseResetTaskTest extends Specification {
+
+
+    def "reset the database when scheduling is enabled"() {
+        given:
+        def repository = Mock(TodoItemRepository)
+        def databaseResetTask = new ScheduledDatabaseResetTask(repository)
+        databaseResetTask.schedulingEnabled = true;
+
+        when:
+        databaseResetTask.resetDatabase()
+
+        then:
+        1 * repository.deleteAll()
+        3 * repository.save(_)
+        1 * repository.findAll()
+    }
+
+    def "do nothing when scheduling is not enabled"() {
+        given:
+        def repository = Mock(TodoItemRepository)
+        def databaseResetTask = new ScheduledDatabaseResetTask(repository)
+        databaseResetTask.schedulingEnabled = false
+
+        when:
+        databaseResetTask.resetDatabase()
+
+        then:
+        0 * repository.deleteAll()
+        0 * repository.save(_)
+    }
+
+}
