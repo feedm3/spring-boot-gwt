068cb4f31a379d5f8bf93df23618ab3b72f87c72
Fabian Dietenberger <fabian.dietenberger@ametras.com>
2016-03-10 08:40:33 +0100

    Put the CORS configuration into the WebMvcConfigurer instead of an filter
    Needs less code and better to understand

--- src/main/java/com/codecrafters/server/SpringBootGwtApplication.java
+++ src/main/java/com/codecrafters/server/SpringBootGwtApplication.java
@@ -2,7 +2,11 @@ package com.codecrafters.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 @EnableScheduling
@@ -10,5 +14,16 @@ public class SpringBootGwtApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootGwtApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                // enabling CORS to make RestyGWT work
+                registry.addMapping("/**").allowedOrigins("*").allowedMethods("*");
+            }
+        };
     }
 }
