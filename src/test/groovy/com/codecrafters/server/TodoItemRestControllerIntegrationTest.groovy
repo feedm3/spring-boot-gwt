603a4b671197e50a4c8bd1b8b75a72f09f31d789
Fabian Dietenberger <piano.fabian@t-online.de>
2018-04-10 13:28:15 +0200

    update all dependencies

--- src/test/groovy/com/codecrafters/server/TodoItemRestControllerIntegrationTest.groovy
+++ src/test/groovy/com/codecrafters/server/TodoItemRestControllerIntegrationTest.groovy
@@ -3,9 +3,9 @@ package com.codecrafters.server
 import com.fasterxml.jackson.core.type.TypeReference
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.context.embedded.LocalServerPort
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.boot.web.server.LocalServerPort
 import org.springframework.http.HttpEntity
 import org.springframework.http.HttpMethod
 import org.springframework.http.HttpStatus
